2670c0932d8430d695ada2ffbf719767409fa912
Kongmeng <kongmengwuwei@163.com>
2026-01-29 18:01:47 +0800

    蓝牙移植

--- project/user/src/main.c
+++ project/user/src/main.c
@@ -7,6 +7,10 @@
 #include "mpu6050.h"
 #include "Motor.h"
 #include "pid.h"
+#include "bluetooth.h"
+
+uint8 data_buffer[32];
+uint8 data_len;
 
 uint8 RunFlag;
 int16 LeftPWM, RightPWM;
@@ -45,6 +49,7 @@ int main(void)
 	 system_delay_ms(300); 
 
 	Buzzer_Init();
+	Bluetooth_Init ();
 	flash_init();
 	key_init(10);
 	Menu_Init();
@@ -58,11 +63,72 @@ int main(void)
 	
 	while (1)
 	{
+//		BlueSerial_SendString("abc");
+//		BlueSerial_SendNumber(32, 2);
+		
+		
+		
 		key_scanner();
 		Menu_Update();
 		Mpu6050_Show();
-		ips200_show_int(0,128,LeftSpeed,5);
-		ips200_show_int(0,144,RightSpeed,5);	  
+		
+		if (BlueSerial_RxFlag == 1)
+		{
+			char *Tag = strtok(BlueSerial_RxPacket, ",");
+			
+			if (strcmp(Tag, "key") == 0)
+			{
+				char *Name = strtok(NULL, ",");
+				char *Action = strtok(NULL, ",");
+				
+			}
+			else if (strcmp(Tag, "slider") == 0)
+			{
+				char *Name = strtok(NULL, ",");
+				char *Value = strtok(NULL, ",");
+				
+				if (strcmp(Name, "AngleKp") == 0)
+				{
+					AnglePID.Kp = atof(Value);
+				}
+				else if (strcmp(Name, "AngleKi") == 0)
+				{
+					AnglePID.Ki = atof(Value);
+				}
+				else if (strcmp(Name, "AngleKd") == 0)
+				{
+					AnglePID.Kd = atof(Value);
+				}
+				else if (strcmp(Name, "SpeedKp") == 0)
+				{
+					SpeedPID.Kp = atof(Value);
+				}
+				else if (strcmp(Name, "SpeedKi") == 0)
+				{
+					SpeedPID.Ki = atof(Value);
+				}
+				else if (strcmp(Name, "SpeedKd") == 0)
+				{
+					SpeedPID.Kd = atof(Value);
+				}
+			}
+			else if (strcmp(Tag, "joystick") == 0)
+			{
+				int8_t LH = atoi(strtok(NULL, ","));
+				int8_t LV = atoi(strtok(NULL, ","));
+				int8_t RH = atoi(strtok(NULL, ","));
+				int8_t RV = atoi(strtok(NULL, ","));
+				
+				SpeedPID.Target = LV / 25.0;
+				DifPWM = RH / 2;
+			}
+			
+			BlueSerial_RxFlag = 0;
+		}
+			ips200_show_int(0,128,(int8)SpeedPID.Kd,5);
+		
+//		ips200_show_int(0,128,LeftSpeed,5);
+//		ips200_show_int(0,144,RightSpeed,5);	  
 //		printf("ENCODER_1 counter \t%d .\r\n", encoder_data_1);                 // 输出编码器计数信息
 //    printf("ENCODER_2 counter \t%d .\r\n", encoder_data_2);                 // 输出编码器计数信息  
       
@@ -83,7 +149,7 @@ void pit_handler(void)
 	//角度环PID
 	if (RunFlag)
 	{
-		AnglePID.Actual = pitch + 3.5;
+		AnglePID.Actual = pitch-2;
 		PID_Update(&AnglePID);
 		AvePWM = -AnglePID.Out;
 		
